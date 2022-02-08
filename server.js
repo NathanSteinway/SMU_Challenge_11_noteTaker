3e74c8bcb6ff1f21743497525aaf1e2d91c5caa5
unknown <steinway.nathan@gmail.com>
2022-02-07 18:55:34 -0600

    added get and post requests fitting assignment guidelines

--- server.js
+++ server.js
@@ -4,6 +4,13 @@
 
 const express = require('express');
 
+//================================================
+//Routes to handle html and api get/post requests
+//================================================
+
+const htmlRoutes = require('./routes/htmlRoutes');
+const apiRoutes = require('./routes/apiRoutes');
+
 //========================================
 // Get the express server up and running!
 //========================================
@@ -11,7 +18,48 @@ const express = require('express');
 const app = express();
 const PORT = process.env.PORT || 3001;
 
+//================================
+// Middleware
+//================================
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.static('public'));
+app.use('/api', apiRoutes);
+app.use('/', htmlRoutes);
+
+//========================
+// Get and post requests.
+//========================
+
+app.get('/notes', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/notes.html'));
+});
+
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/index/html'));
+});
+
+// Had to JSON.parse because db.json notes don't display properly for some reason?
+
+app.get('/notes', (req, res) => {
+    const data = fs.readFileSync('./db/db.json');
+    res.json(JSON.parse(data));
+});
+
+// using uuid here to assign each note with a randomized id
+
+app.post('/notes', (req, res) => {
+    const notes = JSON.parse(fs.readFileSync('./db/db.json'));
+    const newNote = req.body;
+    newNote.id = uuid.v4();
+    notes.push(newNote);
+    fs.writeFileSync('./db/db.json', JSON.stringify(notes));
+    res.json(notes);
+});
+
+
 app.listen(PORT, function() {
     console.log(`Listening on port ${PORT}`);
-})
+});
 
